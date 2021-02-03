fcaf13eec0814cf73254c4ddeaf6bf90b2b735dd
Mark Kelly <big.mako@gmail.com>
2021-02-02 23:34:31 -0800

    updated custom button styling and added sign-in sign-out into header for usersAuth status

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,12 +49,12 @@ class SignIn extends React.Component {
             label='Password'
             required
           />
-
+          <div className='buttons'>
           <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {' '}
-            Sign in with Google{' '}
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign in with Google
           </CustomButton>
+          </div>
         </form>
       </div>
     );
