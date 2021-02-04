6adcb9522fd55afbaa68ac8e760d5476fac6e40a
Mark Kelly <big.mako@gmail.com>
2021-02-04 00:50:16 -0800

    created sign up component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -21,8 +21,24 @@ export const createUserProfileDocument = async (userAuth, additionData) => {
     const userRef = (firestore.doc(`users/${userAuth.uid}`));
 
     const snapShot = await userRef.get();
-    console.log(snapShot);
-}
+    
+    if(!snapShot.exist) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+};
 
 firebase.initializeApp(config);
 
