0139a3526c1aa278af610ec88a0cb275b0037740
Mark Kelly <big.mako@gmail.com>
2021-02-05 17:36:46 -0800

    addded styling to collection items and add to cart button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-
-import './collection-item.styles.scss';
+import React from "react";
+import CustomButton from '../../components/custom-button/custom-button.component';
+import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div className='image' style={{backgroundImage: `url(${imageUrl})`
-    }} />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
+  <div className="collection-item">
+    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
-)
+    <CustomButton inverted> Add to cart </CustomButton>
+  </div>
+);
 
 export default CollectionItem;
